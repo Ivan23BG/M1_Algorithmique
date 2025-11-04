b663d62d2b3c5c12d685fd570fa79a3cfa18b851
Ivan23BG <ilejeune@outlook.com>
2025-11-04 14:32:29 +0100

    solution ex2 q3

--- src/HAI710I_Intelligence_Artificielle/tps/scripts/parametrised_urbcsp.sh
+++ src/HAI710I_Intelligence_Artificielle/tps/scripts/parametrised_urbcsp.sh
@@ -1,4 +1,12 @@
-for i in 200 155 110 65 20
+nb_vars = 10
+nb_contr = 10
+nb_vals = 15
+nb_tuples = 30
+nb_res = 3
+
+# naming the files by tuple, name them by multiple
+# variables if using several for loops
+for tuple in 200 155 110 65 20
 do
-    ./urbcsp 10 15 10 $i 10 > csp$i.txt
+    ./urbcsp $nb_vars $nb_vals $nb_contr $tuple $nb_res > csp$tuple.txt
 done
