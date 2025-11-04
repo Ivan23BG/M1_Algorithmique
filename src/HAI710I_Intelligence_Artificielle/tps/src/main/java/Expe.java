60a5cdce4b1b52953dee8b85e8810b832ad41198
Ivan23BG <ilejeune@outlook.com>
2025-11-04 14:29:14 +0100

    solution ex2 q1

--- src/HAI710I_Intelligence_Artificielle/tps/src/main/java/Expe.java
+++ src/HAI710I_Intelligence_Artificielle/tps/src/main/java/Expe.java
@@ -34,6 +34,8 @@ public class Expe {
 		String files_to_read[] = new String[] {"benchSatisf.txt", "benchInsat.txt"};
 		// String ficName = "bench.txt";
 		int nbRes=3;
+		int nb_success = 0;
+		double nb_total = 0;
 		for (String ficName : files_to_read) {
 			
 		BufferedReader readFile = new BufferedReader(new FileReader(ficName));
@@ -46,16 +48,29 @@ public class Expe {
 			System.out.println("Réseau lu dans "+ficName+" numero "+nb+" :\n"+model+"\n\n");
 
 			
+			model.getSolver().limitTime("10s");
 			// Calcul de la première solution
 			if(model.getSolver().solve()) {
-			System.out.println("\n\n*** Première solution ***");        
-			System.out.println(model);
+				// System.out.println("\n\n*** Première solution ***");        
+				// System.out.println(model);
+				// on a trouvé une solution donc 
+				// on augmente de 1 le nombre de reussites et de modeles
+				nb_success += 1;
+				nb_total += 1;
+			} else if (model.getSolver().isStopCriterionMet()) {
+				System.out.println("The solver could not find a solution nor prove that none exists in the given time");
+			} else {
+				System.out.println("The solver has proved the problem has no solution");
+				// on a trouvé un modele qui n'a pas de solution
+				nb_total += 1;
 			}
 
 			// Affichage de l'ensemble des caractéristiques de résolution
 			// System.out.println("\n\n*** Bilan ***");        
 			// model.getSolver().printStatistics();
 		}
+		double nb_reussites_percent = nb_success / nb_total * 100;
+		System.out.println("Total de reussites: "+nb_reussites_percent+"%");
 		}
 		return;	
 	}
