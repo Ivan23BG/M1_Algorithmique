62a06d02bdd612d1d663c0e0469142dcc29a597a
Ivan23BG <ilejeune@outlook.com>
2026-01-20 09:32:11 +0100

    added utility file to generate multiple exercise type files at once

---
+++ templates/generate_files.py
@@ -0,0 +1,25 @@
+import os
+
+template = """% ----- Consignes exo {xx} ----- %
+\\begin{{td-exo}}[title]\\, % {xx} 
+	
+\\end{{td-exo}}
+
+% ----- Solutions exo {xx} ----- %
+\\iftoggle{{showsolutions}}{{ 
+	\\begin{{td-sol}}[]\\ % {xx}
+		
+	\\end{{td-sol}}
+}}{{}}
+"""
+
+for i in range(1, 36):
+    if i == 2:
+        continue
+
+    filename = f"exo_{i:02d}.tex"
+    if not os.path.exists(filename):
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(template.format(xx=i))
+
+print("Done.")
