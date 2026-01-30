8376c4598984cbc87bc3ec43d1bb5c35181962af
Ivan23BG <ilejeune@outlook.com>
2026-01-30 11:02:30 +0100

    minor + temporary changes + calcul formel exos 1-3, + layout for all tds + sage code beginning

---
+++ src/HAI807I_Calcul_Formel/tps/exo04_f01.py
@@ -0,0 +1,9 @@
+def poly_add(A, B, R):
+    n = max(len(A), len(B))
+    res = [R(0)] * n
+    for i in range(n):
+        a = A[i] if i < len(A) else 0
+        b = B[i] if i < len(B) else 0
+        res[i] += a + b
+    res = normalise(res, R)
+    return res
