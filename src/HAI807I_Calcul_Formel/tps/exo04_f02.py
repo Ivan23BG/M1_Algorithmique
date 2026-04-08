9d6c70727e3ef90f5bcf57cf72c2538ec15e7078
Ivan23BG <ilejeune@outlook.com>
2026-04-08 13:09:27 +0200

    1. compiled pdfs 2. refactored code theme logic 3. refactored some dependencies back into common header 4. deleted calcul formel since it's been moved to another repository

--- src/HAI807I_Calcul_Formel/tps/exo04_f02.py
+++
@@ -1,7 +0,0 @@
-def poly_mult(A, B, R):
-    res = [R(0)] * (len(A) + len(B) - 1)
-    for i in range(len(A)):
-        for j in range(len(B)):
-            res[i + j] += A[i] * B[j]
-    res = remove_zeroes(res, R)
-    return res
--- src/HAI807I_Calcul_Formel/tps/exo04_f03.py
+++
@@ -1,5 +0,0 @@
-def remove_zeroes(L, R):
-    L = list(L)
-    while len(L) > 0 and L[-1] == R(0):
-        L.pop()
-    return L
